7008a9bf5a7f12f675893f30f04928173d9eeeee
Tiago Funk <tiagoff.tf@gmail.com>
2017-08-13 23:23:42 -0300

    Terminei a lista diversa de revisão

--- 0_Revisão/Exercícios/Projeto_Lista_Diversa/src/Executar/Executar.java
+++ 0_Revisão/Exercícios/Projeto_Lista_Diversa/src/Executar/Executar.java
@@ -45,50 +45,46 @@ public class Executar {
                     Questao07 q07 = new Questao07();
                     q07.executar();
                     break;
-//                case 8:
-//                    Questao08 q08 = new Questao08();
-//                    q08.executar();
-//                    break;
-//                case 9:
-//                    Questao09 q09 = new Questao09();
-//                    q09.executar();
-//                    break;
-//                case 10:
-//                    Questao10 q10 = new Questao10();
-//                    q10.executar();
-//                    break;
-//                case 11:
-//                    Questao11 q11 = new Questao11();
-//                    q11.executar();
-//                    break;
-//                case 12:
-//                    Questao12 q12 = new Questao12();
-//                    q12.executar();
-//                    break;
-//                case 13:
-//                    Questao13 q13 = new Questao13();
-//                    q13.executar();
-//                    break;
-//                case 14:
-//                    Questao14 q14 = new Questao14();
-//                    q14.executar();
-//                    break;
-//                case 15:
-//                    Questao15 q15 = new Questao15();
-//                    q15.executar();
-//                    break;
-//                case 16:
-//                    Questao16 q16 = new Questao16();
-//                    q16.executar();
-//                    break;
-//                case 17:
-//                    Questao17 q17 = new Questao17();
-//                    q17.executar();
-//                    break;
-//                case 18:
-//                    Questao18 q18 = new Questao18();
-//                    q18.executar();
-//                    break;
+                case 8:
+                    Questao08 q08 = new Questao08();
+                    q08.executar();
+                    break;
+                case 9:
+                    Questao09 q09 = new Questao09();
+                    q09.executar();
+                    break;
+                case 10:
+                    Questao10 q10 = new Questao10();
+                    q10.executar();
+                    break;
+                case 11:
+                    Questao11 q11 = new Questao11();
+                    q11.executar();
+                    break;
+                case 12:
+                    Questao12 q12 = new Questao12();
+                    q12.executar();
+                    break;
+                case 13:
+                    Questao13 q13 = new Questao13();
+                    q13.executar();
+                    break;
+                case 14:
+                    Questao14 q14 = new Questao14();
+                    q14.executar();
+                    break;
+                case 15:
+                    Questao15 q15 = new Questao15();
+                    q15.executar();
+                    break;
+                case 16:
+                    Questao16 q16 = new Questao16();
+                    q16.executar();
+                    break;
+                case 17:
+                    Questao17 q17 = new Questao17();
+                    q17.executar();
+                    break;
                 case -1:
                     break;
                 default:
